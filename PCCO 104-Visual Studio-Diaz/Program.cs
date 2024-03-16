e0da8f1143e280a763db490714626f5d85bfd040
R Castro <castro1330046@mls.ceu.edu.ph>
2024-03-16 11:05:10 +0800

    Add project files.

---
+++ PCCO 104-Visual Studio-Diaz/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PCCO_104_Visual_Studio_Diaz
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
